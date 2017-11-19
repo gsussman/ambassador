db86dfde59bd8c902e3721f93aa2b12be4bdbc31
Gene Sussman <gene.sussman@yale.edu>
2017-11-19 04:30:13 +0000

    new signup

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^howitworks/$', views.hiw, name='hiw'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from signup.forms import EmailSignupForm
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from frontpage.forms import SignupForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -16,6 +18,24 @@ def home(request):
     else:
     	form = EmailSignupForm()
     	return render(request, 'frontpage/home.html', {'form': form})
+
+def signup(request):
+    if request.method == "POST":
+        form = request.POST
+        print 'form is post'
+        for key, values in request.POST.lists():
+            print(key, values)
+        name = form['name']
+        highschool = form['hsInput']
+        major = form['majorInput']
+        activites = form['activitesInput']
+        colleges = form.getlist('schoolInput')
+        send_mail('Ambassador - New Signup', 'Here is the message.', 'info@findambassador.com', ['gene.sussman@gmail.com'], fail_silently=False)
+        messages.success(request, 'Thank you for signing up! We will let you know when we offically launch!') 
+        return HttpResponseRedirect('/signup/')
+    else:
+    	form = EmailSignupForm()
+    	return render(request, 'frontpage/signup.html', {'form': form})
 
 def hiw(request):
 
